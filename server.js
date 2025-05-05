678425011875101a8e8e5ae5740ff57b5536720a
Vivek Kumar <vk803690@gmail.com>
2025-05-05 20:38:23 +0530

    Remove node_modules from Git tracking

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const httpServer = createServer(app);
 const io = new Server(httpServer, {
     cors: {
         origin: 'https://befit.vibushdigital.com',
-        methods: ['GET', 'POST'],
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
     },
 });
 
